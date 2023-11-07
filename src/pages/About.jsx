8f4532ffa4ca8db733ec0f21a10392d688e92d6e
BDubOne <bryan.t.bartell@gmail.com>
2023-11-07 08:18:36 -0600

    working on changing dependencies

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,80 +1,44 @@
 import SeasonalCarousel from '../components/SeasonalCarousel'
-import GetEpi from '../components/GetEpi'
 import {useState, useEffect} from 'react'
 import axios from 'axios'
-
-const AboutPage = () =>{
-    const [selectedSeason, setSelectedSeason] = useState('s01')
-    const [characters, setCharacters] = useState([])
+const AboutPage = () => {
     const [episodes, setEpisodes] = useState([]);
+    
+    useEffect(() => {
+        const getEpisodes = async () => {
+            try {
+                const episodeResponse = await axios.get('https://rickandmortyapi.com/api/episode')
+                const episodesData = episodeResponse.data.results
 
-        const handleSeasonChange = (event) => {
-            setSelectedSeason(event.target.value);
-            console.log(episodes.episodes)
-
+                const episodesWithImages = [];
+                for (const episode of episodesData) {
+                    const charactersResponse = await axios.get(episode.characters[5]);
+                    const characterData = charactersResponse.data;
+                    const episodeData = {
+                        backgroundImage: characterData.image,
+                        title: episode.name,
+                        releaseDate: episode.air_date,
+                        episodeNumber: episode.episode,
+                    };
+                    episodesWithImages.push(episodeData);
+                }
+            
+                setEpisodes(episodesWithImages);
+            } catch (error) {
+                console.error('error fetching episode', error);
+            }
         }
-        useEffect(() => {
-            const fetchEpisodes = async() => {
-                try {
-                    const response = await axios.get('https://rickandmortyapi.com/api/episode');
-                    setEpisodes(response.data.results);
-                    console.log(response.data.results)
-                } catch (error) {
-                    console.error('no episodes here', error);
-                }
-                }
-            const fetchCharacters = async () => {
-                try {
-                    const response = await axios.get('https://rickandmortyapi.com/api/character');
-                    const characterImages = response.data.results.map((character) => character.image);
-                    setCharacters(characterImages);
-                    console.log(characterImages)
-                } catch (error) {
-                    console.error('no images here', error);
-                }
-                }
-        fetchEpisodes();
-        fetchCharacters();
-        
-      
-    }, []);
-        
-    return(
+        getEpisodes();
+    })
+    return (
         <div>
-        <h2 className='text-white'>About Rick and Morty</h2>
-        <label className='text-white'>Select a Season:</label>
-        <select value={selectedSeason} onChange={handleSeasonChange}>
-            <option value="s01">Season 1</option>
-            <option value="s02">Season 2</option>
-            
-        </select>
-        {episodes.length > 0 && characters.length > 0 && (
-            <SeasonalCarousel
-            episodes={episodes.filter((episode) => episode.episode.includes(selectedSeason))}
-            characters={characters}
-            />
-        )}
-        </div>
-        );
-}
-export default AboutPage
-        {/* <GetEpi>
-       
-        {({ episodes }) => {
-            handleCharacterData(episodes);
+        <h1>
+            About
+        </h1>
+        <SeasonalCarousel episodes={episodes} />
 
-            return (
-            <SeasonalCarousel 
-                episodes={episodes.filter(episode =>
-                episode.episode.includes(selectedSeason))}
-                characters={characters}
-              
-                />             
-            );           
-        }}
-         </GetEpi>
         </div>
-    );
-            
+    )
 }
-export default AboutPage; */}
+
+export default AboutPage
--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,31 +1,64 @@
 //api pull for each character'
-import GetInfo from '../components/GetChar';
 import Cards from '../components/Card'
+import axios from 'axios'axios
+import { useState, useEffect } from 'react'
 
 
 
 
 const Persons = () =>{
-    const { characters } = GetInfo();
+    const [characters, setCharacters] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+   useEffect(() => {
+    const getCharacters = async () => {
+        try {
+            const response = await axios.get('https://rickandmortyapi.com/api/character');
+            const totalPages = response.data.info.pages;
+            const allCharacters = [];
+            for (let page=1; page <= totalPages; page++) {
+                const pageResponse = await axios.get(`https://rickandmortyapi.com/api/character?page=${page}`);
+                allCharacters.push(...pageResponse.data.results);
+            }
+            setCharacters(allCharacters);
+            setIsLoading(false);
+        } catch (error) {
+            console.error('Error fetching characters', error)
+            setIsLoading(false);
+        }
+    };
+
+    getCharacters();
+   }, [])
+
 
     return (
+        <div>
+        {isLoading ? (
+            <p>Loading...</p>
+        ) : (
+      
         <>
-        <div className='min-h-screen flex justify-center grid grid-cols-1 grid rows-2'>
-        <h2 className="text-white">Rick and Morty Characters</h2>
-        <div className='grid grid-cols-3 gap-4'>
-        {characters.map((character, idx) => (
+        <h1>
+            Characters
+        </h1>
+        <div className="row">
+        {characters.map((character, idx) =>(
+        <div key={idx} className="col-md-4 mb-4">
         <Cards
-            key={idx}
-            name={character.name}
-            image={character.image}
-            origin={character.origin.name}
+        key={idx}
+        name={character.name}
+        image={character.image}
+        species={character.species}
+        oritin={character.origin.name}
         />
+        </div>
         ))}
         </div>
-        </div>
         </>
-    
-    );
-};
+        )}
+        </div>
+    )
+}
 
 export default Persons;
